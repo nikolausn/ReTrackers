22aef54b42c29aeb45d46f804d4845ba03a291cf
Dan Stillman <dstillman@zotero.org>
2013-08-04 13:18:58 -0400

    Use local variable rather than global const for loader

--- chrome/content/helloworldzotero/include.js
+++ chrome/content/helloworldzotero/include.js
@@ -1,6 +1,6 @@
 // Only create main object once
 if (!Zotero.HelloWorldZotero) {
-	const loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+	let loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
 					.getService(Components.interfaces.mozIJSSubScriptLoader);
 	loader.loadSubScript("chrome://helloworldzotero/content/hello.js");
 }
