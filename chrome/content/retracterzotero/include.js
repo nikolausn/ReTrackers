d4d35fc31138ced4480410b49a1b8ede0d74678b
nikolausn <nikolaus.nova@gmail.com>
2019-03-28 14:55:15 -0500

    remove retractionwatch local db, only use pubmed now

--- chrome/content/retracterzotero/include.js
+++ chrome/content/retracterzotero/include.js
@@ -2,6 +2,6 @@
 if (!Zotero.RetracterZotero) {
 	loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
 					.getService(Components.interfaces.mozIJSSubScriptLoader);
-    loader.loadSubScript("chrome://retracterzotero/content/list_retracted.js");
+    //loader.loadSubScript("chrome://retracterzotero/content/list_retracted.js");
 	loader.loadSubScript("chrome://retracterzotero/content/retracter.js");
 }
